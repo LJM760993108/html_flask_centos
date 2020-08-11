812c6072cda409fc93236da7882873c76ae1c514
LJM760993108 <760993108@qq.com>
2020-08-11 13:53:32 +0800

    first_commit

---
+++ flask_test/flask_t1.py
@@ -0,0 +1,20 @@
+from flask import Flask
+# import sys
+# sys.exit()
+
+app = Flask(__name__)
+
+# s = " 欢迎 来到 叶丁豪的 网站 ...."
+# @app.route('/')
+# def adddemo1():
+#     return render_template('/views/adddemo1.html') #这里的views是template下的文件夹，html在views下
+
+
+@app.route("/")
+def hello_world():
+    return 'hello_world'
+# def xiaoqi():
+#     return app.send_static_file('../test.html')
+
+if __name__ == "__main__":
+    app.run()
